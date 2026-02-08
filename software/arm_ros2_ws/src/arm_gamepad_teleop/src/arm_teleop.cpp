a486789aea718a10aa48f7e0a5f074cff6e3a84c
Ethan Pham <ethansaddy@gmail.com>
2026-02-08 13:35:57 -0800

    Improve controls
    Added deadman switch, bi-directional angle control, fixed checking button and axes counts, tuned servo singularity settings

--- software/arm_ros2_ws/src/arm_gamepad_teleop/src/arm_teleop.cpp
+++ software/arm_ros2_ws/src/arm_gamepad_teleop/src/arm_teleop.cpp
@@ -7,16 +7,21 @@
 
 using namespace std::chrono_literals;
 
+constexpr int AXIS_COUNT = 8;
+constexpr int BUTTON_COUNT = 14;
+
 class ArmTeleop : public rclcpp::Node {
   public:
     ArmTeleop() : Node("arm_teleop") {
       // Declare parameters
-      x_axis_     = this->declare_parameter("axes.x", 0);
-      y_axis_     = this->declare_parameter("axes.y", 1);
-      z_axis_     = this->declare_parameter("axes.z", 5);
-      roll_axis_  = this->declare_parameter("axes.roll", 3);
-      pitch_axis_ = this->declare_parameter("axes.pitch", 4);
-      yaw_axis_   = this->declare_parameter("axes.yaw", 2);
+      x_axis_               = this->declare_parameter("axes.x", 0);
+      y_axis_               = this->declare_parameter("axes.y", 1);
+      z_axis_               = this->declare_parameter("axes.z", 5);
+      roll_axis_            = this->declare_parameter("axes.roll", 6);
+      pitch_axis_           = this->declare_parameter("axes.pitch", 7);
+      yaw_negative_button_  = this->declare_parameter("buttons.yaw_negative", 0);
+      yaw_positive_button_  = this->declare_parameter("buttons.yaw_positive", 2);
+      deadman_switch_       = this->declare_parameter("axes.deadman", 4); // Button/axis that MUST BE HELD for any movement to occur
 
       this->declare_parameter("max_linear_velocity", 0.3);   // Meters per second
       this->declare_parameter("max_angular_velocity", 0.5);  // Radians per second
@@ -43,14 +48,23 @@ class ArmTeleop : public rclcpp::Node {
       double x      = get_axis_value(msg, x_axis_);
       double y      = -get_axis_value(msg, y_axis_);
       double z      = get_axis_value(msg, z_axis_);
-      double roll   = get_axis_value(msg, roll_axis_);
-      double pitch  = get_axis_value(msg, pitch_axis_);
-      double yaw    = get_axis_value(msg, yaw_axis_);
 
-      // Normalize triggers to be 0 at resting instead of 1
-      // Only for axes that use triggers (3 and 4 for PS4)
-      roll = normalize_trigger_value(roll);
-      pitch = normalize_trigger_value(pitch);
+      // These come from dpad
+      double roll   = get_axis_value(msg, roll_axis_);
+      // RCLCPP_INFO(get_logger(), "Joystick roll: %.2f", roll);
+      double pitch  = get_axis_value(msg, pitch_axis_);
+      // RCLCPP_INFO(get_logger(), "Joystick pitch: %.2f", pitch);
+
+      // This comes from buttons, set default first
+      constexpr double YAW_SPEED = 0.4; // rad/s
+      double yaw    = 0.0;
+
+      // Set yaw
+      if (get_button_value(msg, yaw_negative_button_) == 1) {
+        yaw = -YAW_SPEED;
+      } else if (get_button_value(msg, yaw_positive_button_) == 1) {
+        yaw = YAW_SPEED;
+      }
       
       // Now convert to velocities using the current max velocities
       double max_linear_vel = this->get_parameter("max_linear_velocity").as_double();
@@ -61,13 +75,19 @@ class ArmTeleop : public rclcpp::Node {
       out_msg.header.frame_id = "wrist_roll_link";
       out_msg.header.stamp    = this->now();
 
-      out_msg.twist.linear.x  = compute_velocity_output(x, max_linear_vel);
-      out_msg.twist.linear.y  = compute_velocity_output(y, max_linear_vel);
-      out_msg.twist.linear.z  = compute_velocity_output(z, max_linear_vel);
-      out_msg.twist.angular.x = compute_velocity_output(roll, max_angular_vel);
-      out_msg.twist.angular.y = compute_velocity_output(pitch, max_angular_vel);
-      out_msg.twist.angular.z = compute_velocity_output(yaw, max_angular_vel);
+      // Check deadman switch (all values 0 unless pressed)
+      bool deadman_pressed = (get_axis_value(msg, deadman_switch_) < -0.5);
 
+      if (deadman_pressed) {
+        out_msg.twist.linear.x  = compute_velocity_output(x, max_linear_vel);
+        out_msg.twist.linear.y  = compute_velocity_output(y, max_linear_vel);
+        out_msg.twist.linear.z  = compute_velocity_output(z, max_linear_vel);
+        out_msg.twist.angular.x = compute_velocity_output(roll, max_angular_vel);
+        out_msg.twist.angular.y = compute_velocity_output(pitch, max_angular_vel);
+        out_msg.twist.angular.z = compute_velocity_output(yaw, max_angular_vel);
+      }
+
+      RCLCPP_DEBUG(get_logger(), "Arm deadman switch pressed: %d", deadman_pressed);
       RCLCPP_DEBUG(get_logger(), "Arm x input: %.2f; x output (m/s): %.2f", x, out_msg.twist.linear.x);
       RCLCPP_DEBUG(get_logger(), "Arm y input: %.2f; y output (m/s): %.2f", y, out_msg.twist.linear.y);
       RCLCPP_DEBUG(get_logger(), "Arm z input: %.2f; z output (m/s): %.2f", z, out_msg.twist.linear.z);
@@ -77,23 +97,27 @@ class ArmTeleop : public rclcpp::Node {
       teleop_pub_->publish(out_msg);
     }
 
-    double get_axis_value(const sensor_msgs::msg::Joy::SharedPtr &msg, int index) {
+    double get_button_value(const sensor_msgs::msg::Joy::SharedPtr &msg, int index) {
       // Check index validity
-      int length = sizeof(msg->axes) / sizeof(msg->axes[0]);
+      int length = BUTTON_COUNT;
       if (index < 0 || index >= length) {
         return 0.0;
       }
 
       // If index is valid:
-      return msg->axes[index];
+      return msg->buttons[index];
     }
 
-    double normalize_trigger_value(double value) {
-      // By default, triggers are at value = 1, 50% = 0, 100% = -1
-      // We are mapping to: default 0, 50% = 0.5, 100% = 1.0
-      //TODO change this to use dpad and other buttons instead of axes if necessary
-      // Or use a directional toggle
-      return (1.0 - value) / 2.0;
+    double get_axis_value(const sensor_msgs::msg::Joy::SharedPtr &msg, int index) {
+      // Check index validity
+      int length = AXIS_COUNT;
+      if (index < 0 || index >= length) {
+        RCLCPP_WARN(get_logger(), "Axis value ignored! index: %d; axis message length: %d", index, length);
+        return 0.0;
+      }
+
+      // If index is valid:
+      return msg->axes[index];
     }
 
     double compute_velocity_output(double raw_value, double max_velocity) {
@@ -121,7 +145,9 @@ class ArmTeleop : public rclcpp::Node {
 
     // Parameters
     int x_axis_, y_axis_, z_axis_;
-    int roll_axis_, pitch_axis_, yaw_axis_;
+    int roll_axis_, pitch_axis_;
+    int yaw_negative_button_, yaw_positive_button_;
+    int deadman_switch_;
 };
 
 int main(int argc, char **argv) {
