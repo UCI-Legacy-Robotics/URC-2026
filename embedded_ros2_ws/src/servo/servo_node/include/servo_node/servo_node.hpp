b891951ab1f28bcdf833a6b09882b92592557a94
Ethan <ethansaddy@gmail.com>
2026-02-16 17:28:54 -0800

    Mock servo implementation
    Initial commit in the same style as the other embedded nodes. Includes params, messages, and velocity->position control conversion.
    
    TODO: implement sending actual control messages to servo driver; possible re-implement event loop

---
+++ embedded_ros2_ws/src/servo/servo_node/include/servo_node/servo_node.hpp
@@ -0,0 +1,53 @@
+#ifndef SERVO_NODE_HPP
+#define SERVO_NODE_HPP
+
+#include <mutex>
+
+#include "servo/msg/control_message.hpp"
+// #include "socket_can.hpp"
+
+#include "rclcpp/clock.hpp"
+#include <rclcpp/rclcpp.hpp>
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+using ControlMessage = servo::msg::ControlMessage;
+
+class ServoNode : public rclcpp::Node {
+public:
+    ServoNode(const std::string& node_name);
+    bool init(/* EpollEventLoop* event_loop */); 
+    void deinit();
+private:
+    // void recv_callback(const can_frame& frame);
+    void subscriber_callback(const ControlMessage::SharedPtr msg);
+    void control_message_callback();
+    void start_velocity_output_timer();
+    float get_pwm_from_position(float input_pos_deg);
+
+    // ROS interface
+    rclcpp::Node::SharedPtr node_;
+    
+    // Node params
+    uint8_t channel_id_;
+    uint16_t initial_angle_deg_;
+    uint16_t min_angle_deg_;
+    uint16_t max_angle_deg_;
+    uint16_t min_pwm_micro_s_;
+    uint16_t max_pwm_micro_s_;
+
+    // Pseudo-velocity control
+    rclcpp::TimerBase::SharedPtr velocity_command_timer_;
+    bool send_stepped_position_control_flag_;
+    double last_velocity_loop_time_seconds_;
+    float current_target_pos_deg_;
+    float current_target_vel_deg_;
+    
+    // EpollEvent subscriber_event_;
+    std::mutex control_message_mutex_;
+    ControlMessage control_message_ = ControlMessage();
+    rclcpp::Subscription<ControlMessage>::SharedPtr subscriber_;
+};
+
+#endif // SERVO_NODE_HPP
