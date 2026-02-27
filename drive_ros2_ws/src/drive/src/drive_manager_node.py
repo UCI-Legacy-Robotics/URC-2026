24f3aed4352f4e45666f18849788271604a3200c
Dhrish Karangula <dhrishreddyk@gmail.com>
2026-02-27 13:26:07 -0800

    add drive manager

--- drive_ros2_ws/src/drive/src/drive_manager_node.py
+++ drive_ros2_ws/src/drive/src/drive_manager_node.py
@@ -2,14 +2,12 @@ import rclpy
 from rclpy.node import Node
 
 from yellowjacket_can.msg import ControlMessage
-
-
-from std_msgs.msg import Int32
+from drive_teleop.msg import DriveControlMessage
 
 class DriveNode(Node):
     def __init__(self):
-        super().__init__('drive_node')
-        self.get_logger().info(f"Started drive node")
+        super().__init__('drive_manager_node')
+        self.get_logger().info(f"Started drive manager node")
         # Init yellowjacket control stuff
         self.fl_pub = self.create_publisher(ControlMessage, 'drive_fl/control_message', 10)
         self.fr_pub = self.create_publisher(ControlMessage, 'drive_fr/control_message', 10)
@@ -19,21 +17,25 @@ class DriveNode(Node):
         self.br_pub = self.create_publisher(ControlMessage, 'drive_br/control_message', 10)
         
         # For other stuff like base station to control
-        self.drive_sub = self.create_subscription(Int32, "drive_control", self.subscriber_callback, 10)
+        self.drive_sub = self.create_subscription(DriveControlMessage, "drive_teleop_node/command", self.subscriber_callback, 10)
         
     def subscriber_callback(self, msg):
         new_msg = ControlMessage()
         new_msg.control_mode = 5
-        new_msg.input_mode = 1
-        new_msg.input_vel = msg.data
+        new_msg.input_mode = 1 # Duty cycle control, change to 0 for velocity control
         
+        new_msg.input_vel = msg.left_input_pwm
         self.fl_pub.publish(new_msg)
-        self.fr_pub.publish(new_msg)
         self.ml_pub.publish(new_msg)
-        self.mr_pub.publish(new_msg)
         self.bl_pub.publish(new_msg)
+        
+        new_msg.input_vel = msg.right_input_pwm        
+        self.fr_pub.publish(new_msg)
+        self.mr_pub.publish(new_msg)
         self.br_pub.publish(new_msg)
-        self.get_logger().info(f"Sent velocity of {msg.data} to drive motors!")
+        
+        self.get_logger().info(f"Sent duty cycle of {msg.left_input_pwm} to left drive motors!")
+        self.get_logger().info(f"Sent duty cycle of {msg.right_input_pwm} to right drive motors!")
 
 
 
